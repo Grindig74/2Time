1a43eac66b4df6592cc77aa89f34dda5c4794a49
Grindig74 <1m99m9@gmail.com>
2025-11-04 15:55:34 +0500

    2time MVP v3

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,2 @@
 import { defineConfig } from 'vite'
-
-export default defineConfig({
-  server: { port: 5173, host: true },
-  build: { outDir: 'dist' }
-})
+export default defineConfig({ server:{port:5173,host:true}, build:{outDir:'dist'} })
